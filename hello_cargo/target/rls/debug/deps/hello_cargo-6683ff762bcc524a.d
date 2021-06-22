47dcb8a5221b05f4340f3bdb732f5db8f69e82b8
shernandezz <santihernandezzapata@gmail.com>
2021-06-22 10:26:10 -0500

    idk

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-6683ff762bcc524a.d
@@ -0,0 +1,5 @@
+/home/santiago/Repositories/rust-projects/hello_cargo/target/rls/debug/deps/hello_cargo-6683ff762bcc524a.rmeta: src/main.rs
+
+/home/santiago/Repositories/rust-projects/hello_cargo/target/rls/debug/deps/hello_cargo-6683ff762bcc524a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-8578770f850ac130.d
@@ -0,0 +1,5 @@
+/home/santiago/Repositories/rust-projects/hello_cargo/target/rls/debug/deps/hello_cargo-8578770f850ac130.rmeta: src/main.rs
+
+/home/santiago/Repositories/rust-projects/hello_cargo/target/rls/debug/deps/hello_cargo-8578770f850ac130.d: src/main.rs
+
+src/main.rs:
